e290bdabc0ac156a8eb5677841f33eb753848154
Rhett Sutphin <rhett@detailedbalance.net>
2012-10-21 12:08:23 -0500

    Beginnings of map page using Leaflet and OSM.

---
+++ source/javascripts/locations.js
@@ -0,0 +1,10 @@
+(function() {
+
+  var map = L.map('map').setView([41.923332, -87.698256], 13);
+
+  L.tileLayer('http://{s}.tile.cloudmade.com/faf2b5539ca34ebcb69982fa1aa206b2/997/256/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+  }).addTo(map);
+
+})()
